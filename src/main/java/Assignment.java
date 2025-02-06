fe01a3997c579b29278fd98a237d24f5b540c50a
Noah-777 <25agustn@retsdstudent.ca>
2025-02-06 17:39:43 +0000

    errors

--- src/main/java/Assignment.java
+++ src/main/java/Assignment.java
@@ -20,11 +20,12 @@ public class Assignment {
     public Assignment() {
         // Students will initialize their variables here
         // Example (students should change these values):
-        myString = null;
-        myInt = 0;
-        myDouble = 0.0;
+        myString = "babababbab";
+        myInt = 234;
+        myDouble = 2.234;
         myBoolean = false;
-        myStringArray = null;
-        myIntArray = null;
+        myStringArray = new String[]{"onesa","asfhdlkjfhd"};
+        myIntArray = new int[]{1,2,3,4};
+        
     }
 }
